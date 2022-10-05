eec4b06c63cf700e14cab3a561c357da5a75c238
Joel <jtoledo@uchicago.edu>
2022-10-05 15:33:45 -0500

    Testing Repo

--- BData_2_image.m
+++ BData_2_image.m
@@ -1,7 +1,7 @@
 function BData_2_image(RData,file_name)
     RData_squeeze = squeeze(RData(:,:,:,1));
-    logcomp = 20*log10(RData_squeeze./max(RData_squeeze,[],'all'));
-    imshow(logcomp,[])
+    logcompress = 20*log10(RData_squeeze./max(RData_squeeze,[],'all'));
+    imshow(logcompress,[])
     colormap gray
-    imwrite(logcomp,file_name)
+    imwrite(logcompress,file_name)
 end
