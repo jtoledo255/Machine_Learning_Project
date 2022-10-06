98687f4e3557c78278aae2590cd6765971bea13a
Joel <jtoledo@uchicago.edu>
2022-10-06 15:43:12 -0500

    New script to process images from .mat files and added US images to Image folder

--- BData_2_image.m
+++ BData_2_image.m
@@ -2,6 +2,8 @@ function BData_2_image(RData,file_name)
     RData_squeeze = squeeze(RData(:,:,:,1));
     logcompress = 20*log10(RData_squeeze./max(RData_squeeze,[],'all'));
     imshow(logcompress,[])
+    maxI = max(logcompress,[],'all');
+    minI = min(logcompress,[],'all');
     colormap gray
-    imwrite(logcompress,file_name)
+    imwrite(mat2gray(logcompress,[minI maxI]),['Images/',file_name,'.png'])
 end
