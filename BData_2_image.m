2e979b5f265dbd24f78e472332fa6d643ad8ae86
Katherine <katherinem@uchicago.edu>
2022-10-25 18:08:31 -0500

    adjusted blank images

--- BData_2_image.m
+++ BData_2_image.m
@@ -5,5 +5,6 @@ function BData_2_image(RData,file_name)
     maxI = max(logcompress,[],'all');
     minI = min(logcompress,[],'all');
     colormap gray
-    imwrite(mat2gray(logcompress,[minI maxI]),['Images/',file_name,'.png'])
+    %imwrite(mat2gray(logcompress,[minI maxI]),['Images/',file_name,'.png'])
+    imwrite(mat2gray(logcompress,[minI maxI]),['Machine_Learning_Project/',file_name,'.png'])
 end
